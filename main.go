09edb0255cb7961aca6988905888dc39df4f0ef0
djKooks <inylove82@gmail.com>
2019-03-12 14:37:40 +0900

    Define default action for terminal

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	_ "fmt"
+	"log"
+	"os"
 
-	_ "github.com/manifoldco/promptui"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	
+	gitti := cli.NewApp()
+	gitti.Action = terminalAction
+
+	err := gitti.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
+func terminalAction(c *cli.Context) error {
+	subject := c.Args().Get(0)
+	switch subject {
+	case "repo":
+		log.Println("selected repo")
+	case "issue":
+		log.Println("selected issue")
+	case "user":
+		log.Println("selected user")
+	default:
+		log.Println("wrong selection")
+	}
+
+	return nil
+}
